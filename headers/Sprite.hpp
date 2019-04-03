8a7ec11f69c7ccebce6111481b51555dd185f927
anichols <adam_nichols@taylor.edu>
2019-04-03 16:14:21 -0400

    Updates to Sprite.hpp

--- headers/Sprite.hpp
+++ headers/Sprite.hpp
@@ -1,10 +1,19 @@
 class Sprite {
 	private:
-			std::vector<String> images; //Store file paths to each image. We need a way to store images in memory for easy access
+			std::vector<std::string> imagePaths; //Store file paths to each image. We need a way to store images in memory for easy access
+			std::vector<*sf::Image> images; //sf::Images are created after image is loaded from a file
 	public:
 			int xOffset;
 			int yOffset;
-			int animationSpeed; //Float instead maybe? IDK
-			int imageIndex; 
+			
+			/*
+				Returns 0 on success -1 on failure. Should declare a new image, load it from a file and place that image in our 
+				images vector
+			*/
+			int addImage(std::string filePath);
+			/*
+				Remove an image from the Sprite based on its' location in the images array
+			*/
+			int removeImage(int index); 
 
 }
