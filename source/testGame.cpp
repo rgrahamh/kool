e180f04b9b594d63ebec3a886292ffa8b0a41109
anichols <adam_nichols@taylor.edu>
2019-04-08 20:11:28 -0400

    Updated TODO.md and the testGame

--- source/testGame.cpp
+++ source/testGame.cpp
@@ -16,19 +16,23 @@ int main(){
 	testObj *firstObject = new testObj(50,50);
 	testObj *secondObject = new testObj(300,300);
 	testObj *thirdObject = new testObj(700,400);
+	Object *base = new Object(700,100);
 
 	//Create Sprite
 	Sprite *firstSprite = new Sprite("resources/pirate.png");
+	Sprite *dolphin = new Sprite("resources/dolphin.png");
 
 	//Assign Sprite to Object
 	firstObject->setSprite(firstSprite);
 	secondObject->setSprite(firstSprite);
 	thirdObject->setSprite(firstSprite);
+	base->setSprite(dolphin);
 
 	//Assign Object to Scene
 	firstScene->addObject(firstObject);
 	firstScene->addObject(secondObject);
 	firstScene->addObject(thirdObject);
+	firstScene->addObject(base);
 
 	//Assign Scene to Engine
 	myEngine.addScene(firstScene);
